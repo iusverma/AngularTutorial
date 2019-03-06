fc760e5063a7234f39bb9101ae8e6854f9c3e8f8
Ayush Verma <verma.ius@gmail.com>
2019-03-06 22:11:02 +1100

    variable resolution in multiple controllers

--- js/multipleController.js
+++ js/multipleController.js
@@ -3,10 +3,17 @@ var app=angular.module('MultiApp',[]);
 // defining first controller
 app.controller('firstController',
     function($scope){
-        $scope.firstControllerName = "First Controller";
+        $scope.controllerName = "First Controller - 1";
     });
 // defining second controller
 app.controller('secondController',
     function($scope){
-        $scope.secondControllerName = "Second Controller";
+        /**
+         * even though we are using same variable
+         * as firstController, but variable is picked
+         * scope wise i.e. if in view controller name
+         * is called insided secondController, it will
+         * print this name.
+         */
+        $scope.controllerName = "Second Controller - 2";
     });
