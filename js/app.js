83936a48d42655d287996aefd88c416eda2a4f4c
Ayush Verma <verma.ius@gmail.com>
2019-03-06 21:27:49 +1100

    Adding different type of mapping and functions.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 angular.module('app', []).controller('ExternalController',
 function($scope)
 {
-    $scope.tutorialName = "Angular Tutorial"
+    $scope.tutorialName = "Angular Tutorial";
+    $scope.owner = "Ayush Verma";
+    $scope.visitorFirstName = "firstName";
+    $scope.visitorLastName = "firstName";
+    $scope.fullName = function(fName,lName){
+        return fName + "-" +lName;
+    };
+    $scope.generateName = function(){
+        return $scope.visitorFirstName + " " +$scope.visitorLastName;
+    };
 });
